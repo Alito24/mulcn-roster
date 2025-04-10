f2009d655095be8d2deda9426132f40b89fde8f1
Alito24 <147553250+Alito24@users.noreply.github.com>
2025-04-10 22:38:40 +0200

    Create sections.js

---
+++ sections.js
@@ -0,0 +1,22 @@
+const sections = [
+    {
+        id: "fnaf1",
+        name: "Five Nights at Freddy's",
+        bannerImage: "banners/fnaf1.png" // 1500x100 image
+    },
+    {
+        id: "fnaf2",
+        name: "Five Nights at Freddy's 2",
+        bannerImage: "banners/fnaf2.png"
+    },
+    {
+        id: "fnaf3",
+        name: "Five Nights at Freddy's 3",
+        bannerImage: "banners/fnaf3.png"
+    },
+    {
+        id: "fnaf4",
+        name: "Five Nights at Freddy's 4",
+        bannerImage: "banners/fnaf4.png"
+    }
+];
